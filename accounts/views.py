e44f4b99faae3fad7c2eba5fe55619a0c975efea
mohsen borjipour <mohsenbjp@gmail.com>
2021-11-22 13:24:16 +0330

    1.9.1400
    akhare fasle ehraze hoviat
    qabl az anjame tamrine payani

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate,login
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login_view(request):
@@ -20,6 +21,17 @@ def login_view(request):
     else:
         return redirect('/')
 
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
 def signup_view(request):
-    return render(request,'accounts/signup.html')
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    form=UserCreationForm()
+    context={'form':form}
+    return render(request,'accounts/signup.html',context)
