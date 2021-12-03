cb54c116fc684bb53b6fcb5f481d4f9c21f46455
mohsen borjipour <mohsenbjp@gmail.com>
2021-12-03 17:33:45 +0330

    tamrine fasle 8
    :)))

--- app1/forms.py
+++ app1/forms.py
@@ -8,6 +8,19 @@ class ContactForm(forms.ModelForm):
         model=Contact
         fields='__all__'
 
+        widgets = {
+        'name':forms.TextInput(attrs={'placeholder':'Enter your name','class':'common-input mb-20 form-control','required':'','type':'text'}),
+        'email':forms.EmailInput(attrs={'placeholder':'Enter email address','required':'','type':'email','class':"common-input mb-20 form-control"}),
+        'subject':forms.TextInput(attrs={'placeholder':'Enter subject','type':'text','class':'common-input mb-20 form-control'}),
+        'message':forms.Textarea(attrs={'class':'common-textarea form-control','name':'message', 'placeholder':'Enter Messege','required':'','rows':19})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['subject'].required = False
+
+
+
 class NewsletterForm(forms.ModelForm):
     class Meta:
         model=Newsletter
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from app1.models import Contact
 from app1.forms import ContactForm,NewsletterForm
+from django.contrib import messages
 
 # Create your views here.
 from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
@@ -18,15 +19,26 @@ def contact(request):
     if request.method == 'POST':
         form=ContactForm(request.POST)
         if form.is_valid():
+            # name=form.cleaned_data.get('name')
+            form=form.save()
+            form.name="unknown"
             form.save()
+            messages.add_message(request, messages.SUCCESS, 'aqa shod.')
+        else:
+            messages.add_message(request, messages.ERROR, 'aqa nashod.')
+
     form=ContactForm()
-    return render(request,'app1/contact.html')
+    return render(request,'app1/contact.html',{'form':form})
 
 def newsletter(request):
     if request.method == 'POST':
         form=NewsletterForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.add_message(request, messages.SUCCESS, 'aqa shodaaa.')
+        else:
+            messages.add_message(request, messages.ERROR, 'aqa nashodaaa.')
+
         return HttpResponseRedirect('/')
     else:
         return HttpResponseRedirect('/')
